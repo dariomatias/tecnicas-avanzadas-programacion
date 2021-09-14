fb53be39b447cbe478be48ac946c33af23119f8b
Dario Piriz <Dario.Piriz@qualitestgroup.com>
2021-09-13 21:42:59 -0300

    TP2 - Tecnicas avanzadas de programacion.

---
+++ Campus Virtual/Negocio/Persona.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Negocio
+{
+    public class Persona
+    {
+        private string Nombre { get; set; }
+        private int Edad { get; set; }
+        private int Dni { get; set; }
+        private string Sexo { get; set; }
+        private double Peso { get; set; }
+        private double Altura { get; set; }
+
+        private const string HOMBRE = "H";
+        private const string MUJER = "M";
+
+        public Persona() { }
+
+        public Persona(int dni, string nombre, int edad, string sexo)
+        {
+            Nombre = nombre;
+            Edad = edad;
+            Dni = dni;
+            Sexo = sexo;
+            Altura = 0;
+            Peso = 0;
+        }
+
+        public Persona(int dni, string nombre = "", int edad = 0, string sexo = HOMBRE, double altura = 0, double peso = 0)
+        {
+            Nombre = nombre;
+            Edad = edad;
+            Dni = dni;
+            Sexo = sexo;
+            Altura = altura;
+            Peso = peso;
+        }
+
+        public bool Validar()
+        {
+            return (!string.IsNullOrEmpty(Nombre)
+                    && !string.IsNullOrEmpty(Sexo)
+                    && Dni != 0
+                    && Edad != 0
+                    && Altura != 0
+                    && Peso != 0);
+        }
+
+        public void Modificar(int dni, string nombre, int edad, string sexo, double altura, double peso)
+        {
+            Nombre = nombre;
+            Edad = edad;
+            Dni = dni;
+            Sexo = sexo;
+            Altura = altura;
+            Peso = peso;
+        }
+
+        public Persona Agregar(int dni, string nombre, int edad, string sexo, double altura, double peso)
+        {
+            Persona p = new Persona(dni, nombre, edad, sexo, altura, peso);
+            return p;
+        }
+
+    }
+}
---
+++ Campus Virtual/Presentacion/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Negocio;
+
+namespace Presentacion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona personaVacia = new Persona();
+            Persona personaMinimosDatos = new Persona(11222333, "Juan", 30);
+            Persona personaDatosCompletos = new Persona(22333444, "Carlos", 28, "H", 1.70, 72.200);
+
+
+        }
+    }
+}
